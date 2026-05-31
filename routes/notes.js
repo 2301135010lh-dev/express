c6175402252e54d3f1c385df5daa4e662286125c
2301135010lh-dev <2301135010lh@cyber-u.ac.jp>
2026-05-31 10:36:40 +0900

    git add dog

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = "mongodb+srv://taka621happysmiler:taka1434521@cluster0.jvejima.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
